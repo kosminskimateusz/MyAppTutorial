0ff0112f56d37473bdfb9896c4fc8a73b4694d99
Mateusz Kośmiński <kosminskimateusz@gmail.com>
2022-01-11 15:49:22 +0100

    Side bar sticked

---
+++ Scripts/ShowHide/ShowHideContentCode.js
@@ -0,0 +1,11 @@
+function showHideCode(id)	{
+	let element = document.getElementById(id);
+	if (element.style.display === 'none')	{
+		element.style.display = 'block';
+	}	else	{
+		element.style.display = 'none';
+	}
+}
+
+// let content = document.getElementById('contentCSS');
+// content.innerHTML = '';
